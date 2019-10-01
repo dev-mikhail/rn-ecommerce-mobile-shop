42fa253ef780fc1911d403f31a1563384a29d23a
Mikhail <mikhail97ru@rambler.ru>
2019-10-02 00:46:29 +0800

    created actions types, product, cart, order

---
+++ webshop/app/redux/actions/productAction.js
@@ -0,0 +1,10 @@
+// app/redux/actions/productAction.js
+import { FETCH_PRODUCTS } from './types';
+import { getProducts } from '../../data';
+export const fetchProducts = () => dispatch => {
+  const books = getProducts();
+  dispatch({
+    type: FETCH_PRODUCTS,
+    payload: books
+  })
+}
